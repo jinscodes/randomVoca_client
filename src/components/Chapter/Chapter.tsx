7f2aefd2d9f548181ac7c606f71d35e12e7561e8
{jinsung} <jayhan0215@gmail.com>
2023-10-31 13:23:11 +0900

    Frame for en_note

--- src/components/Chapter/Chapter.tsx
+++ src/components/Chapter/Chapter.tsx
@@ -1,12 +1,30 @@
+import { Dispatch, SetStateAction } from "react";
 import { DBDatas } from "types/types";
 import st from "./Chapter.module.scss";
 
 interface Prop {
   dbDatas: DBDatas[];
+  setMatch: Dispatch<SetStateAction<string>>;
 }
 
-const Chapter = ({ dbDatas }: Prop) => {
-  return <section className={st.chapter}>Chapter</section>;
+const Chapter = ({ dbDatas, setMatch }: Prop) => {
+  const handleClick = (target: string) => {
+    setMatch(target);
+  };
+
+  return (
+    <section className={st.chapter_container}>
+      {dbDatas.map((el, idx) => (
+        <button
+          key={idx}
+          className={st.chapter}
+          onClick={() => handleClick(`${el.title}|${el.chapter}`)}
+        >
+          {el.title} - {el.chapter}
+        </button>
+      ))}
+    </section>
+  );
 };
 
 export default Chapter;
