ba4215fea211358b39e85ec3c2aac62ba5b26baf
{jinsung} <jayhan0215@gmail.com>
2023-11-09 15:25:55 +0900

    fix Login validation error

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -22,7 +22,11 @@ const Login = () => {
     })
       .then((res) => res.json())
       .then((data) => {
-        setCookies("login", data.token);
+        if (data.status === 200) {
+          setCookies("login", data.token);
+        } else {
+          alert("아이디 또는 비밀번호가 잘못되었습니다. 다시 입력해주세요");
+        }
       });
   };
 
