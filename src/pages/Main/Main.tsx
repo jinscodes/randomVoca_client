64f50fda423d124b383e9cb07041ec00d3aa291c
{jinsung} <jayhan0215@gmail.com>
2023-11-10 11:04:56 +0900

    Home & Main

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,5 +1,26 @@
+import Note from "components/Note/Note";
+import { useEffect, useState } from "react";
+import { DBDatas } from "types/types";
+
 const Main = () => {
-  return <div>Main</div>;
+  const [dbDatas, setDbDatas] = useState<DBDatas[]>();
+  const [match, setMatch] = useState<string>("");
+  const [isLogin, setIsLogin] = useState(false);
+
+  useEffect(() => {
+    fetch("http://localhost:8000/")
+      .then((response) => response.json())
+      .then((payload) => {
+        setDbDatas([]);
+        setDbDatas(payload);
+      });
+  }, []);
+
+  dbDatas && console.log(dbDatas);
+
+  return (
+    <section>{dbDatas && <Note dbDatas={dbDatas} match={match} />}</section>
+  );
 };
 
 export default Main;
