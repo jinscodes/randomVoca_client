c0ce715bab35078566ffb64612ba7c31231c9f6b
{jinsung} <jayhan0215@gmail.com>
2023-11-15 09:22:41 +0900

    Post request

--- src/pages/Regist/Regist.tsx
+++ src/pages/Regist/Regist.tsx
@@ -1,6 +1,25 @@
 import st from "./Regist.module.scss";
 
 const Regist = () => {
+  const registVoca = async () => {
+    console.log("clicked");
+
+    await fetch("http://localhost:8000/regist", {
+      method: "POST",
+      body: JSON.stringify([
+        {
+          id: "jins99",
+          title: "경선식",
+          chapter: "ch1",
+          words_en: ["test", "test2"],
+          words_ko: ["테스트", " 테스트2"],
+          correct: 0,
+          wrong: 0,
+        },
+      ]),
+    }).then((res) => res.json());
+  };
+
   return (
     <section className={st.regist_container}>
       <table>
@@ -14,6 +33,7 @@ const Regist = () => {
             <input />
           </td>
         </tr>
+        <button onClick={() => registVoca()}>button</button>
       </table>
     </section>
   );
