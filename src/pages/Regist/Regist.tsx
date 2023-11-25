2bd66926d35f153c4d3cfe1c6be2c5bcbcc8875d
{jinsung} <jayhan0215@gmail.com>
2023-11-26 02:26:44 +0900

    EN & KO are added to wordArr

--- src/pages/Regist/Regist.tsx
+++ src/pages/Regist/Regist.tsx
@@ -1,52 +1,91 @@
-import { useState } from "react";
+import { ChangeEvent, useState } from "react";
 import st from "./Regist.module.scss";
 
 interface AddToList {
-  en: string;
-  ko: string;
+  en?: string | undefined;
+  ko?: string | undefined;
 }
 
 const Regist = () => {
-  const [wordArr, setWordArr] = useState<AddToList[]>();
+  const [wordArr, setWordArr] = useState<AddToList[]>([]);
+  const [word, setWord] = useState<AddToList>({
+    en: "",
+    ko: "",
+  });
 
-  const AddToList = (target: AddToList) => {
-    wordArr && setWordArr((prev) => prev?.concat(target));
-  };
-  const registVoca = async () => {
-    try {
-      await fetch("http://localhost:8000/regist", {
-        method: "POST",
-        body: JSON.stringify({
-          id: "id test",
-          title: "title test",
-          chapter: "chapter test",
-        }),
+  const AddToList = (newObj: AddToList) => {
+    console.log(word);
+
+    if (word.en !== "" && word.ko !== "") {
+      wordArr && setWordArr((prev) => prev?.concat(newObj));
+      setWord({
+        en: "",
+        ko: "",
       });
-    } catch (error) {
-      console.log(error);
-      alert("Fail");
+    } else {
+      alert("There is a empty place");
     }
   };
 
-  console.log(wordArr);
+  // const registVoca = async () => {
+  //   try {
+  //     await fetch("http://localhost:8000/regist", {
+  //       method: "POST",
+  //       body: JSON.stringify({
+  //         id: "id test",
+  //         title: "title test",
+  //         chapter: "chapter test",
+  //       }),
+  //     });
+  //   } catch (error) {
+  //     console.log(error);
+  //     alert("Fail");
+  //   }
+  // };
+
+  wordArr.length > 0 && console.log(wordArr);
 
   return (
     <section className={st.regist_container}>
       <div className={st.form_container}>
         <div className={st.input_container}>
-          <input type="text" placeholder="EN" />
-          <input type="text" placeholder="KO" />
+          <input
+            type="text"
+            placeholder="EN"
+            value={word.en}
+            onChange={(event: ChangeEvent<HTMLInputElement>) => {
+              const inputValue = event.currentTarget.value;
+              setWord((prev) => ({
+                ...prev,
+                en: inputValue,
+              }));
+            }}
+          />
+          <input
+            type="text"
+            placeholder="KO"
+            value={word.ko}
+            onChange={(event: ChangeEvent<HTMLInputElement>) => {
+              const inputValue = event.currentTarget.value;
+              setWord((prev) => ({
+                ...prev,
+                ko: inputValue,
+              }));
+            }}
+          />
         </div>
-        <button>Add</button>
+        <button onClick={() => AddToList(word)}>Add</button>
       </div>
       <table>
         <th>
-          <td></td>
-          <td></td>
+          {wordArr.map((el) => (
+            <td>{el.en}</td>
+          ))}
         </th>
         <tr>
-          <td></td>
-          <td></td>
+          {wordArr.map((el) => (
+            <td>{el.ko}</td>
+          ))}
         </tr>
       </table>
       <button>Submit</button>
