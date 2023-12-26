895d961d43b8da00d707ca582bba85ec1c18e3e8
{jinsung} <jayhan0215@gmail.com>
2023-12-26 09:32:04 +0900

    Signup Check

--- src/components/Signup/Check.tsx
+++ src/components/Signup/Check.tsx
@@ -1,5 +1,26 @@
-export const Check = () => {
-  return <div>Check</div>;
+import { ValuesType } from "types/types";
+import st from "./Check.module.scss";
+
+interface Props {
+  values: ValuesType;
+}
+
+export const Check = ({ values }: Props) => {
+  console.log(values);
+
+  return (
+    <div className={st.check_container}>
+      <div>{values.lastname}</div>
+      <div>{values.firstname}</div>
+      <div>{values.birth.month}</div>
+      <div>{values.birth.date}</div>
+      <div>{values.birth.year}</div>
+      <div>{values.gender}</div>
+      <div>{values.email}</div>
+      <div>{values.id.id}</div>
+      <div>{values.id.pw}</div>
+    </div>
+  );
 };
 
 export default Check;
